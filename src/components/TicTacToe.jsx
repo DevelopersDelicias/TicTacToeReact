9fd2a3b7a03b8a35fd1b8e09e8587ddb40b29528
Benjamin Cisneros Barraza <cisnerosbarraza@gmail.com>
2022-03-16 15:50:19 -0700

    Create custom hook useGame

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -1,62 +1,18 @@
-import React, { useEffect, useState } from 'react'
-import { getWinner } from '../helpers/game.helpers'
+import React from 'react'
+import useGame from '../hooks/useGame.hook'
 import Board from './Board'
 import Stats from './Stats'
 import ToolBar from './ToolBar'
 
-const NO_PLAYER = null
-const PLAYER_1 = { id: 1, mark: 'X', bgColor: '#dc685a' }
-const PLAYER_2 = { id: 2, mark: 'O', bgColor: '#ecaf4f' }
-
-const NUMBER_OF_POSITIONS = 9
-const MINIMUM_PLACED_MARKS_TO_WIN = 5
-const DEFAULT_BOARD = new Array(NUMBER_OF_POSITIONS).fill(NO_PLAYER)
-const DEFAULT_PLAYER = PLAYER_1
-
 const TicTacToe = () => {
-  const [board, setBoard] = useState(DEFAULT_BOARD)
-  const [nextPlayer, setNextPlayer] = useState(PLAYER_1)
-  const [winner, setWinner] = useState(NO_PLAYER)
-  const marksInBoard = board.filter(s => s != NO_PLAYER).length
-
-  useEffect(() => {
-    if (
-      marksInBoard >= MINIMUM_PLACED_MARKS_TO_WIN &&
-      marksInBoard < NUMBER_OF_POSITIONS
-    ) {
-      const theWinner = getWinner(board)
-
-      if (theWinner) {
-        setWinner(theWinner)
-        setNextPlayer(NO_PLAYER)
-      }
-    }
-
-    if (marksInBoard === NUMBER_OF_POSITIONS) {
-      alert('DRAW GAME!')
-      handleOnReset()
-    }
-  }, [board])
-
-  const handleSelectPlayer = number => {
-    const selectedPlayer = board[number - 1]
-
-    if (selectedPlayer != NO_PLAYER || winner != NO_PLAYER) {
-      return
-    }
-    const modifiedBoard = [...board]
-    modifiedBoard[number - 1] = nextPlayer
-
-    setBoard(modifiedBoard)
-    setNextPlayer(prev => (prev.id === PLAYER_1.id ? PLAYER_2 : PLAYER_1))
-  }
-
-  const handleOnReset = () => {
-    setBoard(DEFAULT_BOARD)
-    setWinner(NO_PLAYER)
-    setNextPlayer(DEFAULT_PLAYER)
-  }
-
+  const {
+    nextPlayer,
+    winner,
+    marksInBoard,
+    board,
+    handleSelectPlayer,
+    handleOnReset,
+  } = useGame()
   return (
     <div className='tic-tac-toe'>
       <Stats
