f39e3cc839c8f47f34e5ad18f316ce50eac30f2e
Benjamin Cisneros Barraza <cisnerosbarraza@gmail.com>
2022-03-23 17:38:40 -0700

    Move label style to the component

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,10 +1,19 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledLabel = styled.label`
+  font-weight: 800;
+  width: 200px;
+  display: inline-block;
+  text-align: right;
+  margin-right: 40px;
+`
 
 const Section = ({ id, label, message }) => {
   return (
     <div className='section' id={id}>
-      <label>{label}</label>
+      <StyledLabel>{label}</StyledLabel>
       <span>{message}</span>
     </div>
   )
