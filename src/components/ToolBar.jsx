874ddf1be93fff5639573b1bac8ab5ca4989e052
Benjamin Cisneros Barraza <cisnerosbarraza@gmail.com>
2022-03-23 17:35:41 -0700

    Move toolbar style to the component

--- src/components/ToolBar.jsx
+++ src/components/ToolBar.jsx
@@ -1,13 +1,19 @@
 import { func } from 'prop-types'
 import React from 'react'
+import styled from 'styled-components'
 
+const StyledDiv = styled.div`
+  margin: 10px auto;
+  padding: 5px 20px;
+  text-align: center;
+`
 const ToolBar = ({ onReset }) => {
   return (
-    <div className='toolbar'>
+    <StyledDiv>
       <button onClick={onReset} className='btn btn-primary'>
         Reset Game
       </button>
-    </div>
+    </StyledDiv>
   )
 }
 
